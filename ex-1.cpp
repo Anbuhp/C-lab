d4cf329dfc353782cbd7bf6cd3eafbf2ad001ec2
Anbuhp <144266572+Anbuhp@users.noreply.github.com>
2024-01-30 06:46:10 +0530

    Create ex-1.cpp

---
+++ ex-1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+class Student
+{
+  public:
+    int id;
+    string name;
+           void insert(int i, string n)
+        
+    {
+                    id = i;
+                    name = n;
+                
+    }
+           void display()    
+        
+    {
+            
+            cout << id << "  " << name << endl;
+            
+        
+    }
+        
+};
+  
+
+    int main(void) 
+{
+      
+    Student s1;
+      //creating an object of Student   
+    Student s2;
+      //creating an object of Student  
+    s1.insert(201, "Sonoo");
+        
+    s2.insert(202, "Nakul");
+        
+    s1.display();
+        
+    s2.display();
+      
+    return 0;
+      
+}
+ 
