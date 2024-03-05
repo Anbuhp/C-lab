ab441e677b18e73dc7a504267f0f2d083ae38e42
Anbuhp <144266572+Anbuhp@users.noreply.github.com>
2024-03-05 12:05:13 +0530

    Create ex6.cpp

---
+++ ex6.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class test
+{
+	private:
+		int num;
+		public:
+			test()
+			{
+				num=8;
+			}
+			void operator++()
+			{
+			num=num+2;
+		}
+void print()
+{
+	cout<<"the count is:"<<num;
+}
+};
+int main()
+{
+	test tt;
+	++tt;
+	tt.print();
+	return 0;
+}
