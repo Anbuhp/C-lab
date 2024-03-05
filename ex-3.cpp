afc3ff81c315c157bc88a8385fa5f103482ec62c
Anbuhp <144266572+Anbuhp@users.noreply.github.com>
2024-03-05 16:36:40 +0530

    Update ex-3.cpp

--- ex-3.cpp
+++ ex-3.cpp
@@ -2,30 +2,33 @@
 
 #include <iostream>
 
+using namespace std;
+
 int display(int x, int y)
- {
-   if( y == 0 ) 
 {
-      throw "Division by zero condition!";
-   }
-   return (x/y);
+   if (y == 0) 
+   {
+      throw "Division by zero condition!";
+   }
+   return (x / y);
 }
 
-int main ()
- {
-   int a = 50;
-   int b = 0;
-   int c = 0;
-   try 
+int main()
 {
-      c = display(a, b);
-      cout << c << endl;
-   } 
-catch (const char* msg)
- {
-      cerr << msg << endl;
-   }
-   return 0;
+   int a = 50;
+   int b = 0;
+   int c = 0;
+   try 
+   {
+      c = display(a, b);
+      cout << c << endl;
+   } 
+   catch (const char* msg)
+   {
+      cout << msg << endl;
+   }
+   return 0;
 }
+
 Output
 Division by zero condition!
