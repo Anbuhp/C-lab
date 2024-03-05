fb5239754bf5513e0da9a9fc5b8eb44b7b0282a0
Anbuhp <144266572+Anbuhp@users.noreply.github.com>
2024-03-05 16:33:13 +0530

    Update ex-2.cpp

--- ex-2.cpp
+++ ex-2.cpp
@@ -1,30 +1,30 @@
 // 2.Constructor & Destructor
 
   
-#include <iostream>  
-class Employee  
- {  
-   public:  
-        Employee()    
-        {    
-            cout<<"Constructor Invoked"<<endl;    
-        }    
+#include<iostream>
+using namespace std;
 
-        ~Employee()    
-        {    
-            cout<<"Destructor Invoked"<<endl;    
-        }  
-};  
-int main(void)   
-{  
-    Employee e1; //creating an object of Employee   
-    Employee e2; //creating an object of Employee  
-    return 0;  
+class Employee
+{
+    public:
+    Employee()
+    {
+        cout<<"Constructor Invoked"<<endl;
+        }
+        ~Employee()
+        {
+            cout<<"Destructor Invoked"<<endl;
+        }
+};
+int main(void)
+{
+    Employee e1; 
+    return 0;
 }
 
 
 Output:
-Constructor Invoked  Constructor Invoked
-Destructor Invoked   Destructor Invoked
+Constructor Invoked
+Destructor Invoked 
 
 
